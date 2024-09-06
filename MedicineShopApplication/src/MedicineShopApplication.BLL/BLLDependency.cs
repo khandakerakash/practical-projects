85776fc8111e7a001085d2e281c7442ccb232d29
Akash <akash.cse10@gmail.com>
2024-09-07 01:00:56 +0600

    Create BLLDependency.cs

---
+++ MedicineShopApplication/src/MedicineShopApplication.BLL/BLLDependency.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace MedicineShopApplication.BLL
+{
+    public static class BLLDependency
+    {
+        public static IServiceCollection AddBLLDependency(this ServiceCollection services)
+        {
+            return services;
+        }
+    }
+}

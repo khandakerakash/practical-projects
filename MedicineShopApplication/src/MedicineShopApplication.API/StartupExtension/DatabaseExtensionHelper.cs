140a4cfd45fabfa8410f54d1e484d7c62cc32118
Akash <akash.cse10@gmail.com>
2024-09-07 01:08:26 +0600

    Create DatabaseExtensionHelper.cs

---
+++ MedicineShopApplication/src/MedicineShopApplication.API/StartupExtension/DatabaseExtensionHelper.cs
@@ -0,0 +1,25 @@
+﻿using MedicineShopApplication.DLL.DbContextInit;
+using Microsoft.EntityFrameworkCore;
+
+namespace MedicineShopApplication.API.StartupExtension
+{
+    public static class DatabaseExtensionHelper
+    {
+        public static IServiceCollection AddDatabaseExtensionHelper(this IServiceCollection services, IConfiguration configuration) 
+        {
+            services.AddDbContext<ApplicationDbContext>(option =>
+                option.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+            return services;
+        }
+
+        public static IApplicationBuilder RunMigration(this IApplicationBuilder app)
+        {
+            using (var scope = app.ApplicationServices.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                db.Database.Migrate();
+            }
+            return app;
+        }
+    }
+}

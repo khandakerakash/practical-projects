f389c8e96072efd91fdea202496556c319ab7cee
Akash <akash.cse10@gmail.com>
2024-09-07 15:10:34 +0600

    Add all repositories in Unit of Work.

--- MedicineShopApplication/src/MedicineShopApplication.DLL/Repositories/ICartRepository.cs
+++ MedicineShopApplication/src/MedicineShopApplication.DLL/Repositories/ICartRepository.cs
@@ -1,10 +1,5 @@
 ﻿using MedicineShopApplication.DLL.DbContextInit;
 using MedicineShopApplication.DLL.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MedicineShopApplication.DLL.Repositories
 {
--- MedicineShopApplication/src/MedicineShopApplication.DLL/UOW/IUnitOfWork.cs
+++ MedicineShopApplication/src/MedicineShopApplication.DLL/UOW/IUnitOfWork.cs
@@ -1,9 +1,19 @@
 ﻿using MedicineShopApplication.DLL.DbContextInit;
+using MedicineShopApplication.DLL.Repositories;
 
 namespace MedicineShopApplication.DLL.UOW
 {
     public interface IUnitOfWork : IDisposable
     {
+        IUserRepository UserRepository { get; }
+        ICategoryRepository CategoryRepository { get; }
+        IProductRepository ProductRepository { get; }
+        ICartRepository CartRepository { get; }
+        ICartItemRepository CartItemRepository { get; }
+        IOrderRepository OrderRepository { get; }
+        IOrderItemRepository OrderItemRepository { get; }
+        IPaymentRepository PaymentRepository { get; }
+        IInvoiceRepository InvoiceRepository { get; }
 
         Task<bool> SaveChangesAsync();
     }
@@ -16,6 +26,35 @@ namespace MedicineShopApplication.DLL.UOW
         {
             _context = context;
         }
+
+        private IUserRepository _userRepository;
+        private ICategoryRepository _categoryRepository;
+        private IProductRepository _productRepository;
+        private ICartRepository _cartRepository;
+        private ICartItemRepository _cartItemRepository;
+        private IOrderRepository _orderRepository;
+        private IOrderItemRepository _orderItemRepository;
+        private IPaymentRepository _paymentRepository;
+        private IInvoiceRepository _invoiceRepository;
+
+        public IUserRepository UserRepository => _userRepository ??= new UserRepository(_context);
+
+        public ICategoryRepository CategoryRepository => _categoryRepository ??= new CategoryRepository(_context);
+
+        public IProductRepository ProductRepository => _productRepository ??= new ProductRepository(_context);
+
+        public ICartRepository CartRepository => _cartRepository ??= new CartRepository(_context);
+
+        public ICartItemRepository CartItemRepository => _cartItemRepository ??= new CartItemRepository(_context);
+
+        public IOrderRepository OrderRepository => _orderRepository ??= new OrderRepository(_context);
+
+        public IOrderItemRepository OrderItemRepository => _orderItemRepository ??= new OrderItemRepository(_context);
+
+        public IPaymentRepository PaymentRepository => _paymentRepository ??= new PaymentRepository(_context);
+
+        public IInvoiceRepository InvoiceRepository => _invoiceRepository ??= new InvoiceRepository(_context);
+
 
         public async Task<bool> SaveChangesAsync()
         {

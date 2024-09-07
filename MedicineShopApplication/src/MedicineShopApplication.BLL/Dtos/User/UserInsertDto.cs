0a69df417a342d58ebdf37dc3c0eaf8a77b256e7
Akash <akash.cse10@gmail.com>
2024-09-08 01:27:33 +0600

    Update UserInsertDto.cs

--- MedicineShopApplication/src/MedicineShopApplication.BLL/Dtos/User/UserInsertDto.cs
+++ MedicineShopApplication/src/MedicineShopApplication.BLL/Dtos/User/UserInsertDto.cs
@@ -8,6 +8,5 @@
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
         public string Address { get; set; }
-        public int CreatedBy { get; set; }
     }
 }

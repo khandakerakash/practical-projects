f97f23fb9891033fdd3ce3b174de55b7276fb69b
Akash <akash.cse10@gmail.com>
2024-09-07 14:04:59 +0600

    Create IUnitOfWork.cs

---
+++ MedicineShopApplication/src/MedicineShopApplication.DLL/UOW/IUnitOfWork.cs
@@ -0,0 +1,39 @@
+﻿using MedicineShopApplication.DLL.DbContextInit;
+
+namespace MedicineShopApplication.DLL.UOW
+{
+    public interface IUnitOfWork : IDisposable
+    {
+
+        Task<bool> SaveChangesAsync();
+    }
+
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            if(disposing)
+            {
+                _context.Dispose();
+                // Dispose others component if needed.
+            }
+        }
+    }
+}

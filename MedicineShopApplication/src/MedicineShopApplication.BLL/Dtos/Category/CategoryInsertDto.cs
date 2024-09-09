be3843ef376d14e8d9cc7bb08dc0116e90a15ebd
Akash <akash.cse10@gmail.com>
2024-09-09 11:31:02 +0600

    ICategoryService is added.

---
+++ MedicineShopApplication/src/MedicineShopApplication.BLL/Dtos/Category/CategoryInsertDto.cs
@@ -0,0 +1,14 @@
+﻿using MedicineShopApplication.BLL.Dtos.Base;
+using MedicineShopApplication.BLL.Dtos.CategoryProduct;
+
+namespace MedicineShopApplication.BLL.Dtos.Category
+{
+    public class CategoryInsertDto : BaseEntityDto
+    {
+        public int CategoryDtoId { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<CategoryProductDto> CategoryProductsDto { get; set; }
+    }
+}
---
+++ MedicineShopApplication/src/MedicineShopApplication.BLL/Services/ICategoryService.cs
@@ -0,0 +1,23 @@
+﻿using MedicineShopApplication.BLL.Dtos.Category;
+using MedicineShopApplication.DLL.UOW;
+
+namespace MedicineShopApplication.BLL.Services
+{
+    public interface ICategoryService
+    {
+        Task<List<CategoryDto>> GetAllCategories();
+        Task<CategoryDto> GetCategoryById(int id);
+        Task<CategoryDto> AddCategory(CategoryInsertDto categoryInsertDto);
+        void UpgradeCategory();
+    }
+
+    public class CategoryService : ICategoryService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CategoryService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+    }
+}

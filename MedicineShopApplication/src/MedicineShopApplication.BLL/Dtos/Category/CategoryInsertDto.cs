08668f5b753c1aa42722372f2160a956abac7c11
Akash <akash.cse10@gmail.com>
2024-09-14 23:22:25 +0600

    Category DTO added for CRUD.

--- MedicineShopApplication/src/MedicineShopApplication.BLL/Dtos/Category/CategoryInsertDto.cs
+++ MedicineShopApplication/src/MedicineShopApplication.BLL/Dtos/Category/CategoryInsertDto.cs
@@ -1,14 +1,13 @@
 ﻿using MedicineShopApplication.BLL.Dtos.Base;
-using MedicineShopApplication.BLL.Dtos.CategoryProduct;
+using MedicineShopApplication.BLL.Dtos.Product;
 
 namespace MedicineShopApplication.BLL.Dtos.Category
 {
     public class CategoryInsertDto : BaseEntityDto
     {
-        public int CategoryDtoId { get; set; }
         public string Code { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public List<CategoryProductDto> CategoryProductsDto { get; set; }
+        public ICollection<ProductDto> ProductDtos { get; set; } = new List<ProductDto>();
     }
 }
--- MedicineShopApplication/src/MedicineShopApplication.BLL/Dtos/Category/CategoryUpdateDto.cs
+++ MedicineShopApplication/src/MedicineShopApplication.BLL/Dtos/Category/CategoryUpdateDto.cs
@@ -3,12 +3,12 @@ using MedicineShopApplication.BLL.Dtos.Product;
 
 namespace MedicineShopApplication.BLL.Dtos.Category
 {
-    public class CategoryDto : BaseEntityDto
+    public class CategoryUpdateDto : BaseEntityDto
     {
         public int CategoryDtoId { get; set; }
         public string Code { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public ICollection<ProductDto> ProductDtos { get; set; } = new List<ProductDto>(); 
+        public ICollection<ProductDto> ProductDtos { get; set; } = new List<ProductDto>();
     }
 }

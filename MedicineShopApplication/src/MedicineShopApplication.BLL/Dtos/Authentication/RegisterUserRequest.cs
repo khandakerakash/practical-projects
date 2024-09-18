e90d6c2dbb2ec7ecffcedfbe0d7f80e3b7ffddf7
Akash <akash.cse10@gmail.com>
2024-09-19 00:41:10 +0600

    Create RegisterUserRequest.cs

---
+++ MedicineShopApplication/src/MedicineShopApplication.BLL/Dtos/Authentication/RegisterUserRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace MedicineShopApplication.BLL.Dtos.Authentication
+{
+    public class RegisterUserRequest
+    {
+        public string Title { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Password { get; set; }
+        public string Address { get; set; }
+    }
+}

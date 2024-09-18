36b8a3da06d65d4142cc7255e763c4df731481fb
Akash <akash.cse10@gmail.com>
2024-09-19 00:30:11 +0600

    Create ApiPaginationResponse.cs

---
+++ MedicineShopApplication/src/MedicineShopApplication.BLL/Dtos/Common/ApiPaginationResponse.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicineShopApplication.BLL.Dtos.Common
+{
+    /// <summary>
+    /// Represents a paginated API response.
+    /// </summary>
+    /// <typeparam name="T">The type of the data returned in the response.</typeparam>
+    public class ApiPaginationResponse<T> : ApiResponse<T>
+    {
+        public int CurrentPage { get; set; }
+        public int PageSize { get; set; }
+        public int TotalPages { get; set; }
+        public int TotalCount { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ApiPaginationResponse{T}"/> class.
+        /// </summary>
+        /// <param name="data">The data returned in the response.</param>
+        /// <param name="currentPage">The current page number.</param>
+        /// <param name="pageSize">The size of each page.</param>
+        /// <param name="totalCount">The total count of items.</param>
+        public ApiPaginationResponse(T date, int currentPage, int pageSize, int totalCount) : base(date)
+        {
+            CurrentPage = currentPage > 0 ? currentPage : 1;
+            PageSize = pageSize > 0 ? pageSize : 10;
+            TotalCount = totalCount >= 0 ? totalCount : 0;
+            TotalPages = (int)Math.Ceiling((double)TotalCount / Math.Max(pageSize, 1));
+        }
+    }
+}

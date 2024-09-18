2d24775c08d4af0c350fdee2daa55e096097bb28
Akash <akash.cse10@gmail.com>
2024-09-19 00:32:58 +0600

    Pagination Request is created.

--- MedicineShopApplication/src/MedicineShopApplication.BLL/Dtos/Common/ApiPaginationResponse.cs
+++ MedicineShopApplication/src/MedicineShopApplication.BLL/Dtos/Common/ApiPaginationResponse.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MedicineShopApplication.BLL.Dtos.Common
+﻿namespace MedicineShopApplication.BLL.Dtos.Common
 {
     /// <summary>
     /// Represents a paginated API response.
---
+++ MedicineShopApplication/src/MedicineShopApplication.BLL/Dtos/Common/PaginationRequest.cs
@@ -0,0 +1,39 @@
+﻿namespace MedicineShopApplication.BLL.Dtos.Common
+{
+    /// <summary>
+    /// Represents a paginated request with sorting capabilities.
+    /// </summary>
+    public class PaginationRequest
+    {
+        // Default values
+        private const int DefaultPage = 1;
+        private const int DefaultPageSize = 10;
+        private const int MaxPageSize = 50;
+
+        private int _page = DefaultPage;
+        private int _pageSize = DefaultPageSize;
+
+        /// <summary>
+        /// Gets or sets the current page number. Defaults to 1.
+        /// </summary>
+        public int Page
+        {
+            get => _page;
+            set => _page = value > 0 ? value : DefaultPage;
+        }
+
+        /// <summary>
+        /// Gets or sets the size of the page. Must be between 1 and 50. Defaults to 10.
+        /// </summary>
+        public int PageSize
+        {
+            get => _pageSize;
+            set => _pageSize = (value > 0) && (value <= MaxPageSize) ? value : DefaultPageSize;
+        }
+
+        /// <summary>
+        /// Gets or sets the property name to sort by. Defaults to an empty string.
+        /// </summary>
+        public string SortBy { get; set; } = string.Empty;
+    }
+}
